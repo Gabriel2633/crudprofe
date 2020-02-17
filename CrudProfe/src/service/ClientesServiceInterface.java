d053a982d28e3294841de3c79ce933b4e4edd7a1
Gabriel2633 <40802589+Gabriel2633@users.noreply.github.com>
2020-02-17 09:26:19 +0100

    Add files via upload

---
+++ CrudProfe/src/service/ClientesServiceInterface.java
@@ -0,0 +1,14 @@
+package service;
+
+import java.util.ArrayList;
+
+import model.Cliente;
+
+public interface ClientesServiceInterface {
+    public void add(Cliente cli);
+    public void update(Cliente cli);
+    public void delete(int id);
+    public Cliente findById(int id);
+    public ArrayList<Cliente> getAll();
+    public ArrayList<Cliente> findByName(String name);
+}
